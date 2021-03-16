045c77d5171eb274ec64ade5f27348ce8c62c55c
YTGachaForeverCorn <79651055+YTGachaForeverCorn@users.noreply.github.com>
2021-03-16 17:20:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var inputs= []
+
+function getParagraph1(){
+for (var i=1; i <=6; i++)
+{
+    inputs.push(document.getElementById("para1_input_box_" + i).value);
+}
+
+inputs.join(". ");
+
+document.getElementById("showParagraph1").innerHTML=inputs.join(". ")
+}
